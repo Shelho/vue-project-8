6d02f9a23929102403ce8522be3f5d9d56f5a29c
shelho <boltievibragim9@gmail.com>
2024-04-24 14:30:57 +0300

    create

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container" >
+    <form class="card" @submit.prevent="createPerson">
+      <h2>Работа с базой данных</h2>
+    
+    <div class="form-control">
+      <label for="name">введите имя</label>
+      <input type="text" id="name" v-model.trim="name">
+    </div>
+    <button class="btn primary" :disabled="name.length === 0">создать человека</button>
+  </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      name:''
+    }
+  },
+  methods:{
+   async createPerson(){ 
+       const response = await fetch('https://vue-with-htpp-c2499-default-rtdb.firebaseio.com/people.json',{
+          method:'POST',
+          headers:{
+            'Content-Type':'application/json'
+          }, 
+          body:JSON.stringify({
+            firsName:this.name
+          })
+        })
+      const firebaseData = await response.json()
+      console.log(firebaseData) 
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
